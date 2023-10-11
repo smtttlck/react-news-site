2cb28602e34eb13b04b0ee040f2ec17b55844374
Samet Tatlıcak <samet_tatlicak@outlook.com>
2023-10-11 16:03:17 +0300

    Update Api.jsx

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,4 +1,4 @@
-const key = "bXwLGQW7rRZXSxDgtoDRGZBb235AAlf8"
+const key = ""
 
 export const fetchData = async page => {
     const url = `https://api.nytimes.com/svc/topstories/v2/${page}.json?api-key=${key}`
